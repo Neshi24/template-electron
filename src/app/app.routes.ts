44ef724c9bd7e9141d35b62587761e7f91df803f
Neshi24 <91468087+Neshi24@users.noreply.github.com>
2025-04-03 11:11:18 +0200

    delay removed
    removing delay on loading, its not needed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,9 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-
   {
     path: '',
     loadComponent: () =>
-      delayImport(() => import('./main/main.component').then(m => m.MainComponent), 1500)
+      import('./main/main.component').then(m => m.MainComponent)
   }
 ];
-
-function delayImport<T>(importFn: () => Promise<T>, delayMs: number): Promise<T> {
-  return new Promise<T>((resolve) => {
-    setTimeout(() => {
-      importFn().then(resolve);
-    }, delayMs);
-  });
-}
-
